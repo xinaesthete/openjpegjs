a1f26315269a3cc4f49e7100113cce3f7a27e2ac
Peter Todd <ptr.tdd@gmail.com>
2025-07-30 15:23:45 +0100

    update build with simd

--- scripts/wasm-build.sh
+++ scripts/wasm-build.sh
@@ -4,7 +4,7 @@ mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
 # enabling simd has no effort as of Oct 9, 2021
 #(cd build && emcmake cmake -DCMAKE_C_FLAGS="-msimd128"  ..) &&
-(cd build && emcmake cmake -DCMAKE_C_FLAGS="-sEXPORT_ES6=1" -DCMAKE_CXX_FLAGS="-sEXPORT_ES6=1" ..) &&
+(cd build && emcmake cmake -DCMAKE_C_FLAGS="-msimd128 -sEXPORT_ES6=1" -DCMAKE_CXX_FLAGS="-msimd128 -sEXPORT_ES6=1" ..) &&
 (cd build && emmake make VERBOSE=1 -j) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist &&
 cp ./build/extern/openjpeg/bin/openjpegjs.wasm ./dist &&
