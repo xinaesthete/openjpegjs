8b03540ed438953dffa4f1b07d73b6df08c5a9f8
Peter Todd <ptr.tdd@gmail.com>
2025-07-30 16:23:56 +0100

    cleanup cmake flags

--- scripts/wasm-build.sh
+++ scripts/wasm-build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-#rm -rf build
+# rm -rf build
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
 # enabling simd has no effort as of Oct 9, 2021. 
 # very marginal difference, but seems slightly faster July 30, 2025
+# these flags are set in the CMakeLists.txt file now
 #(cd build && emcmake cmake -DCMAKE_C_FLAGS="-msimd128"  ..) &&
-(cd build && emcmake cmake -DCMAKE_C_FLAGS="-msimd128" -DCMAKE_CXX_FLAGS="-msimd128 -sEXPORT_ES6=1" ..) &&
+(cd build && emcmake cmake ..) &&
 (cd build && emmake make VERBOSE=1 -j) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist &&
 cp ./build/extern/openjpeg/bin/openjpegjs.wasm ./dist &&
